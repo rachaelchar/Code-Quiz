07e871cb3a7a4c1fc835de9cc10653ad58d8273a
RachaelChar <charewicz.r@gmail.com>
2019-10-14 15:01:22 -0400

    add questions for quiz

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "If you are looking for a typed response from the user, you might use a __________",
+        choices: ["while loop", "prompt", "alert", "for loop"],
+        answer: "prompt"
+    },
+    {
+        title: "In JavaScript, which of these would correctly refer to the class of an HTML element?",
+        choices: ["#class", ".class", "&class", "$class"],
+        answer: ".class"
+    },
+    {
+        title: "To verify that something's value and type is not equal to another's, which might you use in your if statement?",
+        choices: ["===", "==", "!=", "!=="],
+        answer: "!=="
+    },
+    {
+        title: "Before beginning a project, you might want to plan out your steps. This is also known as:",
+        choices: ["pseudo-coding", "scaffolding", "pre-coding", "drafting"],
+        answer: "pseudo-coding"
+    },
+    {
+        title: "How many columns is the Bootstrap model based on?",
+        choices: ["10", "8", "16", "12"],
+        answer: "12"
+    }
+];
