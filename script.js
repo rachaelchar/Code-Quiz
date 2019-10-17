be76327ac097c175315e8486c8b42e19ff9a3ec5
RachaelChar <charewicz.r@gmail.com>
2019-10-17 08:52:26 -0400

    add input form and submit btn for user's initials

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // define variables
 var highScoresList = "";
+var newHighScore = "";
 var $viewScoresBtn = $("#viewScoresBtn");
 var $startBtn = $("#startQuizBtn");
 var $timerElement = $("#timer");
@@ -29,11 +30,39 @@ $(document).ready(function() {
                 // stop the countdown
                 clearInterval(timerInterval);
                 // create a new h1 that says "all done!"
-                var $newTitleElement = $("<h1>All done!</h1>");
+                var $newTitleElement = $("<div><h1>All done!</h1></div>");
                 // append the h1 to the content div
                 $contentDiv.append($newTitleElement);
+                // create a new element telling the user their final score
+                var $newSubElement = $("<div><p>Your final score is " + secondsLeft + ".</p></div>");
+                // append the p element to the content div
+                $newTitleElement.append($newSubElement);
+                // create a new input element within a form. Use input type="submit" so that the button will work with the input field
+                // Source: https://stackoverflow.com/questions/290215/difference-between-input-type-button-and-input-type-submit
+                var $newInputElement = $("<div><p>Enter intials: <form><input id='initials'></input> <input type='submit' id='newSubmitButton'></button></form></p></div>");
+                // append the input element
+                $newTitleElement.append($newInputElement);
+                
+                // create a new submit button
+                $(newSubmitButton).on("click", function(event){
+                    // prevent default, otherwise it will reload the page after clicking submit
+                    event.preventDefault();                    
+                    // set a variable to the input ID
+                    var $initials = $("#initials"); 
+                    // get the value of the user's input and turn it into a string 
+                    var $newInputElementString = JSON.stringify($initials.val());
+                    // store the string
+                    localStorage.setItem("inputValue", $newInputElementString);
+                    // concatenate the new string with the number of seconds left to create the high score
+                    newHighScore = $newInputElementString + " - " + secondsLeft;
+                    // console.log to check that it worked -- it did!
+                    console.log(newHighScore);
+              
+                });
+
+
             }
-        }, 1000);
+        }, 10);
 
 
 
